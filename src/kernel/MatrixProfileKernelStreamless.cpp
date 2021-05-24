98bac6dc1a20ab431cbac3b280cf5cd4c2c749df
Jan Scheerer <scheerer@atschulz7.informatik.tu-muenchen.de>
2021-05-24 19:13:42 +0200

    Cyclic Array-Partition for ColumnAggregate

--- src/kernel/MatrixProfileKernelStreamless.cpp
+++ src/kernel/MatrixProfileKernelStreamless.cpp
@@ -174,9 +174,10 @@ void MatrixProfileKernelTLF(const data_t *T, data_t *MP, index_t *MPI) {
 
     data_t rowAggregate[sublen]; index_t rowAggregateIndex[sublen];
 
+    // factor=3 required for fadd and fmul (update if data_t changes)
     data_t columnAggregate[sublen]; index_t columnAggregateIndex[sublen];
-    #pragma HLS ARRAY_PARTITION variable=columnAggregate complete
-    #pragma HLS ARRAY_PARTITION variable=columnAggregateIndex complete
+    #pragma HLS ARRAY_PARTITION variable=columnAggregate cyclic factor=3
+    #pragma HLS ARRAY_PARTITION variable=columnAggregateIndex cyclic factor=3
 
     PrecomputationProcessingElement(T, mu, df, dg, inv, QT, P, rowAggregate, rowAggregateIndex, columnAggregate, columnAggregateIndex);
 
@@ -206,7 +207,7 @@ void MatrixProfileKernelTLF(const data_t *T, data_t *MP, index_t *MPI) {
             // 				<==> column <= row + m/4 [(row <= column, m > 0) ==> row - -m/4 <= column]
             //              <==> row + k <= row + m/4
             //              <==> k <= m/4
-            constexpr size_t column = row + k;
+            const size_t column = row + k;
             bool exclusionZone = k <= m / 4;
             if(!exclusionZone && P[k] > columnAggregate[column]){
                 columnAggregate[column] = P[k];
