cc19e1a5fe6b52e23d903225e6b7d483be3910d5
Jan Luca Scheerer <scheerer@cs.tum.edu>
2021-05-31 16:25:56 +0200

    Further work on Testing

--- test/TestStream1DKernel.cpp
+++ test/TestStream1DKernel.cpp
@@ -1,5 +1,7 @@
 #include "MatrixProfileTests.hpp"
 
+#include <array>
+
 // wrapper struct to have multiple Stream1DKernel instances
 // with different confugrations (data_t, index_t, n, m)
 // this allows for multiple test (without having to recompile)
@@ -10,7 +12,13 @@ struct Stream1DKernel {
 };
 
 TEST(TestStream1DKernel, TestSmall8Syn) {
-    EXPECT_EQ(0, 0);
+    using data_t = double; using index_t = int;
+    constexpr size_t n = 8;
+    constexpr size_t m = 5;
+    std::array<data_t, n> T{1,4,9,16,25,36,49,64};
+    std::array<data_t, n - m + 1> MP;
+    std::array<index_t, n - m + 1> MPI; 
+    Stream1DKernel<data_t, int, n, m>().MatrixProfileKernelTLF(T.data(), MP.data(), MPI.data());
 }
 
 TEST(TestStream1DKernel, TestSmall16Syn) {
--- test/include/MockStream.hpp
+++ test/include/MockStream.hpp
@@ -1,12 +1,28 @@
 #pragma once
 
-#include <cstdlib>
+#include <iostream>
+#include <queue>
 
 namespace mock {
 
     template<typename T, size_t depth>
     class stream {
-        
+        private:
+            std::queue<T> m_queue;
+        public:
+            void write(const T element) {
+                m_queue.push(element);
+            }
+            T read(){
+                if(!m_queue.size()){
+                    // TODO: Throw Error here!
+                    std::cout << "Trying to read from empty stream!" << std::endl;
+                    return T();
+                }
+                T result{m_queue.front()};
+                m_queue.pop();
+                return result;
+            }
     };
 
 };
