f0e6aedc75a23cb400a0d5fe403a033fd39df3b6
Jan Luca Scheerer <scheerer@cs.tum.edu>
2021-06-02 14:02:36 +0200

    Switched Travis Compiler

--- include/Initialize.hpp
+++ include/Initialize.hpp
@@ -1,14 +1,18 @@
 
 #pragma once
 
-template<typename T>
-static constexpr T AggregateInit();
+namespace MatrixProfileConfig {
+    
+    template<typename T>
+    static constexpr T AggregateInit();
 
-template<>
-static constexpr double AggregateInit() { return -1e12; }
+    template<>
+    static constexpr double AggregateInit() { return -1e12; }
 
-template<typename T>
-static constexpr index_t IndexInit();
+    template<typename T>
+    static constexpr index_t IndexInit();
 
-template<>
-static constexpr int IndexInit() { return -1; }
+    template<>
+    static constexpr int IndexInit() { return -1; }
+
+}
