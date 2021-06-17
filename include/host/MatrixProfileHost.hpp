fb280f584fc73a8fc9ac43a51d95c492ff12bfa3
Jan Luca Scheerer <scheerer@cs.tum.edu>
2021-06-17 14:30:01 +0200

    Update MatrixProfileHost.hpp

--- include/host/MatrixProfileHost.hpp
+++ include/host/MatrixProfileHost.hpp
@@ -11,5 +11,5 @@
 static const std::string KernelTLF{"MatrixProfileKernelTLF"};
 static const std::string versionName{
     "\n****** Matrix Profile HLS\n  **** Host-Application (C++/OpenCL) (build " + std::string{__DATE__} + " " 
-    + std::string{__TIME__} + ")" + "\n    ** Kernel (" + KernelTLF + ") [" + KERNEL_IMPL_NAME + "]"
+    + std::string{__TIME__} + ")" + "\n    ** Kernel (" + KernelTLF + ") [" + KERNEL_IMPL_NAME + "]\n"
 };
