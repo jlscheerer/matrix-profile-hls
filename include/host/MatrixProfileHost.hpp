1f22d41b6b8f2045a7161b4e1cdf89f4c69f9788
Jan Luca Scheerer <jscheerer@alveo0.ethz.ch>
2021-06-17 15:00:18 +0200

    More verbose version information

--- include/host/MatrixProfileHost.hpp
+++ include/host/MatrixProfileHost.hpp
@@ -10,6 +10,11 @@
 
 static const std::string KernelTLF{"MatrixProfileKernelTLF"};
 static const std::string versionName{
-    "\n****** Matrix Profile HLS\n  **** Host-Application (C++/OpenCL) (build " + std::string{__DATE__} + " " 
+    "\n****** Matrix Profile HLS (target = " + std::string{TARGET_TYPE_NAME} + ")\n  **** Host-Application (C++/OpenCL) (build " + std::string{__DATE__} + " " 
     + std::string{__TIME__} + ")" + "\n    ** Kernel (" + KernelTLF + ") [" + KERNEL_IMPL_NAME + "]\n"
+    "     * Configuration (n = " + std::to_string(n) + ", m = " + std::to_string(m)
+    #if KERNEL_IMPL_INDEX == 2
+      + ", t = " + std::to_string(t)
+    #endif
+    + ") [data_t = " + DATA_TYPE_NAME + ", index_t = " + INDEX_TYPE_NAME + "]\n"
 };
