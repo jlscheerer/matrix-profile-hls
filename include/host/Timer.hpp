437b0ad74db3073d11db404d4c13d2e2537a5b86
Jan Luca Scheerer <scheerer@cs.tum.edu>
2021-06-29 09:27:39 +0200

    [ci skip] Bug-fix Timer.hpp

--- include/host/Timer.hpp
+++ include/host/Timer.hpp
@@ -22,6 +22,7 @@ class Timer{
 // Source[Modified]: https://stackoverflow.com/questions/22590821/convert-stdduration-to-human-readable-time
 std::ostream& operator<<(std::ostream& os, std::chrono::nanoseconds ns){
     using namespace std::chrono;
+    auto ns_count = ns.count();
     auto h = duration_cast<hours>(ns);
     ns -= h;
     auto m = duration_cast<minutes>(ns);
@@ -38,7 +39,7 @@ std::ostream& operator<<(std::ostream& os, std::chrono::nanoseconds ns){
         os << std::setw(h.count() ? 2 : 1) << m.count() << "m ";
     if (h.count() || m.count() || s.count())
         os << std::setw(h.count() || m.count() ? 2 : 1) << s.count() << "s ";
-    os << std::setw(h.count() || m.count() || s.count() ? 3 : 1) << ms.count() << "ms (" << ns.count() << " ns)";
+    os << std::setw(h.count() || m.count() || s.count() ? 3 : 1) << ms.count() << "ms (" << ns_count << " ns)";
 
     os.fill(fill);
     return os;
