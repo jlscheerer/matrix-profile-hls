058007abacf0e36e6a711006510895d8cdfbcb0a
Jan Luca Scheerer <jscheerer@alveo0.ethz.ch>
2021-07-19 16:44:17 +0200

    Further changes to support other data types

--- include/host/HostSideComputation.hpp
+++ include/host/HostSideComputation.hpp
@@ -10,7 +10,7 @@ namespace HostSideComputation {
 
     void PrecomputeStatistics(std::array<double, n> &T, std::array<data_t, n - m + 1> &QT, std::array<ComputePack, n - m + 1> &data) {
         // Calculate the initial mean, then update using moving mean.
-        double mean = std::accumulate(T.begin(), T.begin() + m, static_cast<data_t>(0)); mean /= m;
+        double mean = std::accumulate(T.begin(), T.begin() + m, 0); mean /= m;
         double prev_mu, mu0 = mean;
 
         // Compute Statistics in single pass through the data
@@ -18,9 +18,9 @@ namespace HostSideComputation {
             prev_mu = mean;
             mean += (i > 0) ? ((T[i + m - 1] - T[i - 1]) / m) 
                             : 0;
-            data[i].df = (i > 0) ? (T[i + m - 1] - T[i - 1]) / 2 
+            data[i].df = (i > 0) ? static_cast<data_t>((T[i + m - 1] - T[i - 1]) / 2)
                             : static_cast<data_t>(0);
-            data[i].dg = (i > 0) ? ((T[i + m - 1] - mean) + (T[i - 1] - prev_mu)) 
+            data[i].dg = (i > 0) ? static_cast<data_t>(((T[i + m - 1] - mean) + (T[i - 1] - prev_mu))) 
                             : static_cast<data_t>(0);
             double qt = 0, inv = 0;
             for (index_t k = 0; k < m; ++k) {
@@ -34,7 +34,7 @@ namespace HostSideComputation {
 
     void PearsonCorrelationToEuclideanDistance(std::array<data_t, n - m + 1> &PearsonCorrelation, std::array<double, n - m + 1> &EuclideanDistance) {
         for (index_t i = 0; i < n - m + 1; ++i)
-            EuclideanDistance[i] = std::sqrt(2 * m * (1 - PearsonCorrelation[i]));
+            EuclideanDistance[i] = std::sqrt(static_cast<double>(2 * m * (1 - PearsonCorrelation[i])));
     }
 
 }
