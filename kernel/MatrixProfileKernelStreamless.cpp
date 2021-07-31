1f82e516929cd41ee89a49b64c10e5deb0b2e607
Jan Luca Scheerer <jscheerer@alveo0.ethz.ch>
2021-07-31 15:00:49 +0200

    Changes to Streamless-Kernel

--- kernel/MatrixProfileKernelStreamless.cpp
+++ kernel/MatrixProfileKernelStreamless.cpp
@@ -15,106 +15,78 @@
 #include "kernel/DataPacks.hpp"
 #include "kernel/TreeReduce.hpp"
 
-void MatrixProfileKernelTLF(const InputDataPack *in, data_t *MP, index_t *MPI) {
-    #pragma HLS INTERFACE m_axi port=in  offset=slave bundle=gmem0
-    #pragma HLS INTERFACE m_axi port=MP  offset=slave bundle=gmem0
-    #pragma HLS INTERFACE m_axi port=MPI offset=slave bundle=gmem1
+void MatrixProfileKernelTLF(const index_t n, const index_t m, const index_t iteration,
+                            const InputDataPack *columns, const InputDataPack *rows, OutputDataPack *result) {
+    #pragma HLS INTERFACE m_axi port=columns offset=slave bundle=gmem0
+    #pragma HLS INTERFACE m_axi port=rows    offset=slave bundle=gmem1
+    #pragma HLS INTERFACE m_axi port=result  offset=slave bundle=gmem2
 
-    data_t QT[n - m + 1];
-    DataPack columnData[n - m + 1], rowData[n - m + 1];
-    
-    // Store computed row aggregates to merge with column 
-    // aggregates during the final reduction stage
-    aggregate_t rowAggregates[n - m + 1];
+    const index_t nOffset = iteration * nColumns;
+    const index_t nRows = n - m + 1 - nOffset;
 
-    MatrixProfileInit:
-    for (index_t i = 0; i < n - m + 1; ++i) {
-       	#pragma HLS PIPELINE II=1
-        const InputDataPack read = in[i];
-        const DataPack data = DataPack{read.df, read.dg, read.inv};
+    // TODO: Comment about QT
+    data_t QT[nColumns];
 
-        // Store read QT value in seperate arary will be updated
-        // during the actual computation
+    constexpr int rowReduceD1 = 8;
+    constexpr int rowReduceD2 = 8;
+    aggregate_t rowReduce[rowReduceD1][rowReduceD2];
+    #pragma HLS ARRAY_PARTITION variable=rowReduce dim=2 complete
+
+#if 0
+    // TODO: Only required for t <= rowReduceD2
+    MatrixProfileInitReduce:
+    for (index_t i = 0; i < rowReduceD1; ++i) {
+        for (index_t j = 0; j < rowReduceD2; ++j) {
+            #pragma HLS UNROLL
+            rowReduce[i][j] = aggregate_t_init;
+        }
+    }
+#endif
+
+    // TODO: Double-Buffer comment
+    aggregate_t columnAggregates[nColumns][2];
+
+    MatrixProfileInitQT:
+    for (index_t i = 0; i < nColumns; ++i) {
+        const index_t columnIndex = nOffset + i;
+        const bool inBounds = columnIndex < n - m + 1;
+        const InputDataPack read = inBounds ? columns[columnIndex]
+                                            : InputDataPack(0);
         QT[i] = read.QT;
-
-        // explicitely store two copies of the input data to
-        // later access both independently in a single cycle 
-        rowData[i] = data;
-        columnData[i] = data;
     }
-
-    // TODO: Comment Breaking Dependency by Introducing "Delay-Buffer"
-    aggregate_t rowReduce[16];
-    #pragma HLS ARRAY_PARTITION variable=rowReduce complete
-
-    // rowReduce needs to be explicitly initialized if n - m + 1 < 16
-    // Because for every row we perform a reduction on all elements!
-    // For every n - m + 1 >= 16 we handle this via implicit initializaiton
-    for (index_t i = 0; i < 16; ++i) {
-	    #pragma HLS UNROLL
-	    rowReduce[i] = aggregate_t_init;
-    }
-
-    // TODO: Comment "Double-Buffer" Write to opposite position than is being read
-    // TODO: Clean-Up Access to columnReduce
-    constexpr int T = 2;
-    aggregate_t columnReduce[n - m + 1][T];
-
-    // =============== [/Compute] ===============
+	
     MatrixProfileComputeRow:
-    for (index_t k = 0; k < n - m + 1; ++k) {
-        MatrixProfileComputeColumn:
-        for (index_t i = 0; i < n - m + 1; ++i) {
-            #pragma HLS PIPELINE II=1
-            const index_t columnIndex = k + i;
-            const bool computationInRange = k + i < n - m + 1;
-            const bool exclusionZone = i < (m / 4);
+    for (index_t i = 0; i < nRows; ++i) {
+        const index_t rowIndex = i;
+        const InputDataPack row = rows[rowIndex];
 
-            const DataPack row = rowData[k];
-            const DataPack column = (!exclusionZone && computationInRange)
-                                            ? columnData[columnIndex] : (DataPack){0, 0, 0};
+        MatrixProfileComputeTile:
+        for (index_t j = 0; j < nColumns; ++j) {
+            const index_t columnIndex = nOffset + i + j;
 
-	        // Update QT value diagonally above via the update formulation
-            // QT_{i, j} = QT_{i-1, j-1} + df_i * dg_j + df_j * dg_i
-            QT[i] += row.df * column.dg + column.df * row.dg;
+            const bool columnInBounds = columnIndex < n - m + 1;
+            const bool exclusionZone = rowIndex > columnIndex - m / 4;
+            const bool inBounds = columnInBounds && !exclusionZone;
 
-            // Calculate PearsonCorrelation
-            // P_{i, j} = QT_{i, j} * inv_i * inv_j
-            const data_t P = QT[i] * row.inv * column.inv;
+            const InputDataPack column = columnInBounds ? columns[columnIndex]
+                                                        : InputDataPack(0);
 
-            // Row-Wise Partial Reduction
-            aggregate_t prevRow = (i < 16) ? aggregate_t_init : rowReduce[i % 16];
-            rowReduce[i % 16] = P > prevRow.value ? aggregate_t(P, columnIndex) : prevRow;
+            QT[j] += row.df * column.dg + column.df * row.dg;
+            const data_t P = inBounds ? QT[j] * row.inv * column.inv : 0;
 
-            // Column-Wise Partial Reduction
-            // Wrap-Around works because if we are not outside 
-            // the exlusionZone P will be 0 and therefore irrelevant
-            aggregate_t prevColumn = (k < T/2) ? aggregate_t_init : columnReduce[columnIndex % (n - m + 1)][k % T];
-	        columnReduce[columnIndex % (n - m + 1)][(k + T/2) % T] = prevColumn.value > P ? prevColumn : aggregate_t(P, k);
+            aggregate_t prevRow = (j < rowReduceD2) ? aggregate_t_init : rowReduce[i % rowReduceD1][j % rowReduceD2];
+	        rowReduce[i % rowReduceD1][j % rowReduceD2] = prevRow.value > P ? prevRow : aggregate_t(P, columnIndex);
+
+            const aggregate_t prevColumn = (j < nColumns - 1 && i > 0) ? columnAggregates[j + 1][i % 2] 
+                                                                       : aggregate_t_init;
+            columnAggregates[j][(i + 1) % 2] = P > prevColumn.value ? aggregate_t(P, rowIndex) 
+                                                                    : prevColumn;
         }
 
-        // Only need to reduce a constant number (16) of 
-        // aggregates via guaranteed TreeReduction
-        rowAggregates[k] = TreeReduce::Maximum<aggregate_t, 16>(rowReduce);
-    }
-    // =============== [/Compute] ===============
-    
-    // =============== [Reduce] ===============
+        const aggregate_t rowAggregate = TreeReduce::Maximum<aggregate_t, rowReduceD2>(rowReduce[i % rowReduceD1]);
+        const aggregate_t columnAggregate = columnAggregates[0][(i + 1) % 2];
 
-    // Compute maximum between Row- and Column-wise aggregates
-    ReductionCompute:
-    for (index_t i = 0; i < n - m + 1; ++i) {
-        #pragma HLS PIPELINE II=1
-	    // Load rowAggregate we computed directly
-        const aggregate_t rowAggregate = rowAggregates[i];
-        // Compute columnAggregate by performing TreeReduction on partial results
-	    const aggregate_t columnAggregate = TreeReduce::Maximum<aggregate_t, T>(columnReduce[i]);
-        // Compute Maximum of both aggregates (i.e. values with lowest EuclideanDistance)
-        const aggregate_t aggregate = rowAggregate.value > columnAggregate.value
-					? rowAggregate : columnAggregate;
-        // Store the Result
-        MP[i] = aggregate.value;
-        MPI[i] = aggregate.index;
+        result[i] = {rowAggregate, columnAggregate};
     }
-    // =============== [/Reduce] ===============
 }
+
